04d9fdb000e39c7cb146da951bafc6c63cb049da
Fikri <mustofa13.fikri@gmail.com>
2020-08-18 11:47:29 +0700

    Make application model

---
+++ database/migrations/2020_08_18_020354_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owners');
+            $table->unsignedBigInteger('scanners');
+            $table->string('name');
+            $table->unsignedInteger('participants');
+            $table->unsignedInteger('max_participants');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
